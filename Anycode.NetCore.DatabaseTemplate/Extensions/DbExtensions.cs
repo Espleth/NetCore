c410f265a78f87747104be36121d5260f57a00a5
Artem Sychev <8123237+Espleth@users.noreply.github.com>
2026-03-30 02:04:45 +0600

    Podman by default, misc fixes

--- Anycode.NetCore.DatabaseTemplate/Extensions/DbExtensions.cs
+++ Anycode.NetCore.DatabaseTemplate/Extensions/DbExtensions.cs
@@ -2,6 +2,9 @@ namespace Anycode.NetCore.DatabaseTemplate.Extensions;
 
 public static class DbExtensions
 {
+	/// <summary>
+	/// Don't use for migrations outside of this project because it'll use wrong assembly
+	/// </summary>
 	public static void AddAppDbContext(this IServiceCollection services, string connectionString)
 	{
 		services.AddAppDbContext<AppDbContext>(connectionString, MapEnums);
