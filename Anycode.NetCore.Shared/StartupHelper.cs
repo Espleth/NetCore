9fbf9cd203a3949e5bf1f358c2002cdd7a3c776c
Artem Sychev <8123237+Espleth@users.noreply.github.com>
2026-04-26 19:22:05 +0600

    Console UTF8

--- Anycode.NetCore.Shared/StartupHelper.cs
+++ Anycode.NetCore.Shared/StartupHelper.cs
@@ -64,6 +64,9 @@ public static class StartupHelper
 		CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
 		CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
 
+		Console.OutputEncoding = Encoding.UTF8;
+		Console.InputEncoding = Encoding.UTF8;
+
 		var assembly = AppDomain.CurrentDomain.FriendlyName;
 		GlobalDiagnosticsContext.Set("Service", assembly);
 
